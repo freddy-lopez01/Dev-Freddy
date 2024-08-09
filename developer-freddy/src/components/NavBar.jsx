2818fccbe1babfd136035af8107cdb8ac7d59332
Freddy Lopez <freddlopez01@gmail.com>
2024-08-09 16:31:37 -0700

    updates

--- developer-freddy/src/components/NavBar.jsx
+++ developer-freddy/src/components/NavBar.jsx
@@ -32,9 +32,6 @@ function NavBar() {
                         <Link to='/projects' className="nav-links" onClick={closeMobileMenu}>Projects</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to='/about' className="nav-links" onClick={closeMobileMenu}>About</Link>
-                    </li>
-                    <li className="nav-item">
                         <Link to='/socials' className="nav-links" onClick={closeMobileMenu}>Gallery</Link>
                     </li>
                 </ul>
